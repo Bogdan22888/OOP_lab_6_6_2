3ba58256b96e906c2a02c4783b4a3fb7f1cf4e95
Роман Данилевич <dwordchanel@gmail.com>
2020-06-06 20:25:20 +0300

    Add project files.

---
+++ OOP_lab_6_6_2/IMetroStation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_lab_6_6_2
+{
+	interface IMetroStation
+	{
+		void ReadName();
+		void ReadYear();
+		void ReadPassAmount();
+		void ReadComment();
+	}
+}
